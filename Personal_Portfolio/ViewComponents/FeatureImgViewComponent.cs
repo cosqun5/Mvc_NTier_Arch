24f8cbe7b8bf694b5cfa7958619c8d3729ed1fea
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2023-12-05 00:42:16 +0400

    finished

---
+++ Personal_Portfolio/ViewComponents/FeatureImgViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Business.Services.Abstract;
+using Entities.Concrate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Personal_Portfolio.ViewComponents
+{
+    public class FeatureImgViewComponent : ViewComponent
+    {
+        private readonly IAboutService _aboutService;
+
+        public FeatureImgViewComponent(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<About> abouts = await _aboutService.GetList();
+            return View(abouts);
+        }
+    }
+}
